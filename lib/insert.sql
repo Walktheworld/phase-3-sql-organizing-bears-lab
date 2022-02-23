cbd35f3454f016610299bae41da969b48849a798
Joe <Walktheworld@me.com>
2022-02-23 13:18:20 -0700

    commit tree 0a49278644e30bc6f61c4e6c1eafdb20cdd32db9

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex) VALUES ("Mr. Chocolate", 2, "M");
+INSERT INTO bears (name, age, sex) VALUES ("Rowdy", 3, "M");
+INSERT INTO bears (name, age, sex) VALUES ("Tabitha", 7, "F");
+INSERT INTO bears (name, age, sex) VALUES ("Sergeant Brown", 4, "M");
+INSERT INTO bears (name, age, sex) VALUES ("Melissa", 2, "F");
+INSERT INTO bears (name, age, sex) VALUES ("Grinch", 1, "M");
+INSERT INTO bears (name, age, sex) VALUES ("Wendy", 5, "F");
+INSERT INTO bears (name, age, sex) VALUES (NULL, 2, "F");
